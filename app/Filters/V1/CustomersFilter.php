d5f3bfd5ccbf2617fc1ef176912ac21569ba479c
Cedjuani <ggwepq@gmail.com>
2024-11-25 03:44:13 +0800

    ✨ - api now supports post, put, and bulk store for authorized users

--- app/Filters/V1/CustomersFilter.php
+++ app/Filters/V1/CustomersFilter.php
@@ -26,6 +26,7 @@ class CustomersFilter extends ParamFilter
         'lte' => '<=',
         'gt' => '>',
         'gte' => '>=',
+        'ne' => '!=',
     ];
 }
 
--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Filters\V1\CustomersFilter;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Requests\V1\StoreCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 use App\Models\Customer;
@@ -35,19 +35,11 @@ class CustomerController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreCustomerRequest $request)
     {
-        //
+        return new CustomerResource(Customer::create($request->all()));
     }
 
     /**
@@ -77,7 +69,7 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
